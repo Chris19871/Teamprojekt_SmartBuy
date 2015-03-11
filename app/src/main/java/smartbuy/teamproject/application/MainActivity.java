3eab4d37915399e408160d4dfe7cf434ed66c372
langhanj <langhanj@hochschule-trier.de>
2015-03-11 16:48:19 +0100

    Aktu. SettingsActivitiy

--- app/src/main/java/smartbuy/teamproject/application/MainActivity.java
+++ app/src/main/java/smartbuy/teamproject/application/MainActivity.java
@@ -1,5 +1,6 @@
 package smartbuy.teamproject.application;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.app.Dialog;
 import android.content.Context;
@@ -13,18 +14,21 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
 
     final Context context = this;
     private Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void onClick() {
+    public void onClick()
+    {
 
         // custom dialog
         final Dialog dialog = new Dialog(context);
@@ -54,31 +58,40 @@ public class MainActivity extends ActionBarActivity {
 
         dialog.show();
     }
+    public void settingsOpen()
+    {
+        final Intent settings = new Intent(this,SettingsActivity.class);
+        startActivity(settings);
+    }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.settings) {
+            settingsOpen();
             return true;
         }
         if (id == R.id.action_add) {
-            onClick();
-            return true;
-        }
+                onClick();
+                return true;
+            }
 
-        return super.onOptionsItemSelected(item);
+            return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ app/src/main/java/smartbuy/teamproject/application/SettingsActivity.java
@@ -0,0 +1,16 @@
+package smartbuy.teamproject.application;
+
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.smartbuy_einstellungen);
+    }
+
+  }
+
